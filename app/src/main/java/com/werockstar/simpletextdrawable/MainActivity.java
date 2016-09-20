715826ca4b707cec6fb8599f99565f9eb21b2485
WeRockStar <vr_freestyle@outlook.com>
2016-09-21 00:31:59 +0700

    Added drawing drawable text

--- app/src/main/java/com/werockstar/simpletextdrawable/MainActivity.java
+++ app/src/main/java/com/werockstar/simpletextdrawable/MainActivity.java
@@ -1,13 +1,22 @@
 package com.werockstar.simpletextdrawable;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        view = findViewById(R.id.view);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackground(new SimpleTextDrawable("Hello Drawable!!!"));
+        }
     }
 }
--- app/src/main/java/com/werockstar/simpletextdrawable/SimpleTextDrawable.java
+++ app/src/main/java/com/werockstar/simpletextdrawable/SimpleTextDrawable.java
@@ -2,28 +2,40 @@ package com.werockstar.simpletextdrawable;
 
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 
 public class SimpleTextDrawable extends Drawable {
 
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private String mMessage;
+
+    public SimpleTextDrawable(String message) {
+        this.mMessage = message;
+        paint.setColor(Color.RED);
+        paint.setTextSize(90);
+    }
+
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.drawText(mMessage, 0, paint.getTextSize(), paint);
     }
 
     @Override
     public void setAlpha(int alpha) {
-
+        invalidateSelf();
     }
 
     @Override
     public void setColorFilter(ColorFilter colorFilter) {
-
+        invalidateSelf();
     }
 
     @Override
     public int getOpacity() {
-        return 0;
+        return PixelFormat.TRANSLUCENT;
     }
 }

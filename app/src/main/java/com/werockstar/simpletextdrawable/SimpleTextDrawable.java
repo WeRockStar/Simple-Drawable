4ee397ba4b10a856f3c8ba0c8d64db477dd31539
WeRockStar <vr_freestyle@outlook.com>
2016-09-21 00:24:44 +0700

    Added SimpleTextDrawable class

---
+++ app/src/main/java/com/werockstar/simpletextdrawable/SimpleTextDrawable.java
@@ -0,0 +1,29 @@
+package com.werockstar.simpletextdrawable;
+
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+public class SimpleTextDrawable extends Drawable {
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
